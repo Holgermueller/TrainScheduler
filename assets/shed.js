8851b10d73935e826de9ecb51017f1b4ceffd030
Holger Mueller <9jholger@gmail.com>
2018-03-29 12:35:42 -0500

    added moment.js and icons

--- assets/shed.js
+++ assets/shed.js
@@ -18,7 +18,35 @@ const database = firebase.database();
 
 //declare variables
 
-//grab info from form
+$("#addTrainBtn").on("click", function(e) {
+    e.preventDefault()
+    let trainInput = $("#trainInput").val().trim();
+        //console.log(trainInput);
+    let destInput = $("#destInput").val().trim();
+        //console.log(destInput);
+    let timeInput = $("#timeInput").val().trim();
+        //console.log(timeInput);
+    let freqInput = $("#freqInput").val().trim();
+        //console.log(freqInput);
 
-//console log form
+    //push info to firebase
+    database.ref().push({
+        name: trainInput,
+        destination: destInput,
+        time: timeInput,
+        frequencty: freqInput,
+    });
+});
 
+//Firebase watcher + initial loader + order/limit Hint
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
+    //store snapshot value
+    let sv = snapshot.val();
+        console.log(sv);
+})
+
+//calculate next arrival
+
+//calculate minutes away
+
+//append everything to schedule
