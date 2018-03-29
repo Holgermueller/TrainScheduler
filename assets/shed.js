b414b0643c40858535a1dd08043c8e976de8f0c4
Holger Mueller <9jholger@gmail.com>
2018-03-29 11:32:24 -0500

    connect to firebase

--- assets/shed.js
+++ assets/shed.js
@@ -2,6 +2,20 @@
 
 //initialize Firebase (those 6/7 lines of code)
 
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyDJYYjJOfgVxjcy3DxPSMsF2QU5lcb3mJg",
+    authDomain: "holger-first-firebase-project.firebaseapp.com",
+    databaseURL: "https://holger-first-firebase-project.firebaseio.com",
+    projectId: "holger-first-firebase-project",
+    storageBucket: "holger-first-firebase-project.appspot.com",
+    messagingSenderId: "343997001344"
+  };
+  firebase.initializeApp(config);
+
+const database = firebase.database();
+
 //declare variables
 
 //grab info from form
