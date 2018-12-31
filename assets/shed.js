ad243191eb70b28acc9603bd286d1fd722426da7
Holger Mueller <9jholger@gmail.com>
2018-12-30 22:16:37 -0500

    removed comments, reorganized files

--- assets/shed.js
+++ assets/shed.js
@@ -1,116 +1,70 @@
 'use strict';
 
-//initialize Firebase (those 6/7 lines of code)
-
-  // Initialize Firebase
-  const config = {
-    apiKey: "AIzaSyDJYYjJOfgVxjcy3DxPSMsF2QU5lcb3mJg",
-    authDomain: "holger-first-firebase-project.firebaseapp.com",
-    databaseURL: "https://holger-first-firebase-project.firebaseio.com",
-    projectId: "holger-first-firebase-project",
-    storageBucket: "holger-first-firebase-project.appspot.com",
-    messagingSenderId: "343997001344"
-  };
-  firebase.initializeApp(config);
+const config = {
+  apiKey: "AIzaSyDJYYjJOfgVxjcy3DxPSMsF2QU5lcb3mJg",
+  authDomain: "holger-first-firebase-project.firebaseapp.com",
+  databaseURL: "https://holger-first-firebase-project.firebaseio.com",
+  projectId: "holger-first-firebase-project",
+  storageBucket: "holger-first-firebase-project.appspot.com",
+  messagingSenderId: "343997001344"
+};
+firebase.initializeApp(config);
 
 const database = firebase.database();
 
 moment();
-//console.log(moment);
 
-//declare variables
+$("#addTrainBtn").on("click", function (e) {
+  e.preventDefault()
 
-$("#addTrainBtn").on("click", function(e) {
-    e.preventDefault()
-    /*figure out where to put thse!!
-    $("#trainInput").empty();
-    $("#destInput").empty();
-    $("#timeInput").empty();
-    $("#freqInput").empty();*/
-    let trainInput = $("#trainInput").val().trim();
-        //console.log(trainInput);
-    let destInput = $("#destInput").val().trim();
-        //console.log(destInput);
-    let timeInput = moment($("#timeInput").val().trim(), "HH:mm").format();
-        //console.log(timeInput);
-    let freqInput = $("#freqInput").val().trim();
-        //console.log(freqInput);
+  let trainInput = $("#trainInput").val().trim();
+  let destInput = $("#destInput").val().trim();
+  let timeInput = moment($("#timeInput").val().trim(), "HH:mm").format();
+  let freqInput = $("#freqInput").val().trim();
 
-    //push info to firebase
-    database.ref().push({
-        name: trainInput,
-        destination: destInput,
-        time: timeInput,
-        frequency: freqInput,
-    });
+  database.ref().push({
+    name: trainInput,
+    destination: destInput,
+    time: timeInput,
+    frequency: freqInput,
+  });
 
-    $("#trainInput").val("");
-    $("#destInput").val("");
-    $("#timeInput").val("");
-    $("#freqInput").val("");
+  $("#trainInput").val("");
+  $("#destInput").val("");
+  $("#timeInput").val("");
+  $("#freqInput").val("");
 });
 
-//create clock and append
 
 let currentTime = moment();
-    console.log(currentTime);
 
 function update() {
-    $("#clock1").html(moment().format("MM-DD-YYYY"))
-    $("#clock2").html(moment().format("HH:mm:ss"));
-    }
-    setInterval(update, 1000);
+  $("#clock1").html(moment().format("MM-DD-YYYY"))
+  $("#clock2").html(moment().format("HH:mm:ss"));
+}
+setInterval(update, 1000);
 
-//Firebase watcher + initial loader + order/limit Hint
-database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
-    //store snapshot value
-    let sv = snapshot.val();
-        //console.log(sv);
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function (snapshot) {
+  let sv = snapshot.val();
+  let trainName = snapshot.val().name;
+  let destName = snapshot.val().destination;
+  let timeName = snapshot.val().time;
+  let freqName = snapshot.val().frequency;
 
-    let trainName = snapshot.val().name;
-    let destName = snapshot.val().destination;
-    let timeName = snapshot.val().time;
-    let freqName = snapshot.val().frequency;
+  let freqParse = parseInt(freqName);
+  let timeMoment = moment(timeName);
+  let diffMinute = moment().diff(timeMoment, "minutes");
+  let timeRemainder = diffMinute % freqParse;
+  let minutesLeft = freqParse - timeRemainder;
 
-        //console.log(trainName);
-        //console.log(destName);
-        //console.log(timeName);
-        //console.log(freqName);
+  let nextTrain = moment().add(minutesLeft, "minutes");
 
-//parse frequency
+  $("#trainTable > tbody").append("<tr id='row'><td>" + trainName + "</td><td>" + destName + "</td><td>" + freqParse +
+    " " + "mins" + "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + minutesLeft +
+    "</td><td><input type='button' id='rmv' class='rmv' value='X'></td></tr>")
 
-let freqParse = parseInt(freqName);
-    //console.log(freqParse);
-
-//calculate minutes away  
-
-let timeMoment = moment(timeName);
-    //console.log(timeMoment);
-
-let diffMinute = moment().diff(timeMoment, "minutes");
-    //console.log(currentTime);
-
-let timeRemainder = diffMinute % freqParse;
-
-let minutesLeft = freqParse - timeRemainder;
-
-//calculate next arrival from military time
-
-let nextTrain = moment().add(minutesLeft, "minutes");
-
-    //console.log(minutesLeft);
-    //console.log(nextTrain.format("HH:mm"));
-
-//append everything to schedule
-
-$("#trainTable > tbody").append("<tr id='row'><td>" + trainName + "</td><td>" + destName + "</td><td>" + freqParse + 
-" " + "mins" +  "</td><td>" + moment(nextTrain).format("HH:mm") + "</td><td>" + minutesLeft + 
-"</td><td><input type='button' id='rmv' class='rmv' value='X'></td></tr>")
-
- //remove button
-
- $('input[type="button"]').click(function(e) {
+  $('input[type="button"]').click(function (e) {
     $(this).closest('tr').remove();
- });
+  });
 
 });
