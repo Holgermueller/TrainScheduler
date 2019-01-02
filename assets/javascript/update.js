73dedc1295823d40578a65520c3125c388972111
Holger Mueller <9jholger@gmail.com>
2019-01-02 13:36:05 -0500

    got both modals to launch properly

--- assets/javascript/update.js
+++ assets/javascript/update.js
@@ -3,36 +3,42 @@
 $(document).on('click', '#update', function () {
   console.log('click');
   $('body').append(`
-  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Update Table Entry</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form >
-          <div class="form-group">
-          <input type="text" id="newTrainName" class="form-group form-control" name="newTrainName" placeholder="Train name...">
-          <input type="text" id="newDestination" class="form-group form-control" name="newDestination" placeholder="Destination...">
-          <input type="text" id="newFirstTrainTime" class="form-group form-control" name="newFirstTrainTime" placeholder="First train time...">
-          <input type="text" id="newFrequency" class="form-group form-control" name="newFrequency" placeholder="Frequency...">
-          <input id="updateSubmit" type="Submit" class="update-submit form-group form-control btn btn-primary" name="update-submit" value="Update">
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-      </div>
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title" id="exampleModalCenterTitle">Update Table Entry</h5>
+      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <form>
+        <div class="form-group">
+          <input type="text" id="newTrainName" class="form-group form-control" name="newTrainName"
+            placeholder="Train name...">
+          <input type="text" id="newDestination" class="form-group form-control" name="newDestination"
+            placeholder="Destination...">
+          <input type="text" id="newFirstTrainTime" class="form-group form-control" name="newFirstTrainTime"
+            placeholder="First train time...">
+          <input type="text" id="newFrequency" class="form-group form-control" name="newFrequency"
+            placeholder="Frequency...">
+          <input id="updateSubmit" type="Submit" class="update-submit form-group form-control btn btn-primary"
+            name="update-submit" value="Update">
+        </div>
+      </form>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
     </div>
   </div>
+</div>
 </div>
   `);
 });
 
-$(document).on('click', '#updateSubmit', function(e) {
+$(document).on('click', '#updateSubmit', function (e) {
   e.preventDefault();
   let updatedTrain = $('#newTrainName').val().trim();
   let newDestination = $('#newDestination').val().trim();
